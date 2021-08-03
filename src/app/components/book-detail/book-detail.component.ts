68d30786b805b03c03554deac7720e493212058f
BenchawanSr <85793785+BenchawanSr@users.noreply.github.com>
2021-08-03 23:22:20 +0700

    Create extensions.json

---
+++ src/app/components/book-detail/book-detail.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CrudService } from './../../service/crud.service';
+import { FormGroup, FormBuilder
+ } from '@angular/forms';
+@Component({
+  selector: 'app-book-detail',
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.css']
+})
+export class BookDetailComponent implements OnInit {
+
+  getID:any;
+  updateForm:FormGroup;
+
+  constructor(
+    public formBuilder : FormBuilder,
+    private router:Router,
+    private ngZone:NgZone,
+    private activatedRoute:ActivatedRoute,
+    private crudService:CrudService
+  ) {
+    this.getID = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.crudService.GetBook(this.getID).subscribe((res)=>{
+      this.updateForm.setValue({
+        name : res['name'],
+        price:res['price'],
+        description:res['description']
+      })
+    })
+
+    this.updateForm = this.formBuilder.group({
+      name : [''],
+      price:[''],
+      description:['']
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  onUpdate():any{
+    this.crudService.updateBook(this.getID,this.updateForm.value).subscribe(()=>{
+      console.log('Data update successfully');
+      this.ngZone.run(()=>this.router.navigateByUrl('/books-list'))
+    },(err)=>{
+      console.log(err);
+    })
+  }
+
+}
